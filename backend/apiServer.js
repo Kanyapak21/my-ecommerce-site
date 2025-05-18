4e15bde77e34e5571e1128cfbfbb554ea6b4e44f
Kanyapak21 <Kanyapak_ngam@cmu.ac.th>
2025-05-18 22:40:27 +0700

    Assignment1

---
+++ backend/apiServer.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3030;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/subject', require('./data/registerOccupation.js'));
+app.use('/api/contact', require('./data/registerContact.js'));
+app.use('/api/register', require('./data/register.js'));
+app.use('/api/login', require('./data/login.js'));
+
+app.listen(PORT, () => {
+    console.log("Server running at http://localhost:" + PORT);
+});
--- backend/expServer.js
+++ backend/expServer.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const cors = require('cors');
-
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
@@ -10,13 +8,14 @@ const PORT = 3000;
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use('/api/register', require('./routes/register.js'));
+
 app.use('/api/subject', require('./routes/subject.js'));
 app.use('/api/contact', require('./routes/contact.js'));
 app.use('/api/subscribe', require('./routes/subscribe.js'));
 
+app.use('/api/users', require('./routes/user.js'));
+
 app.listen(PORT, () => {
-    //console.log("Server running at http://localhost:$(POST)");
     console.log("Server running at http://localhost:"+PORT);
     
 })
